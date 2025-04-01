8a1346598a9c728d18ec58af637318995a3e776d
Elmer <infcle@gmail.com>
2025-03-31 23:21:08 -0400

    feacture: menu de volver al sistema

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers\Filament;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\NavigationItem;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -23,6 +24,12 @@ class AdminPanelProvider extends PanelProvider
     public function panel(Panel $panel): Panel
     {
         return $panel
+            ->navigationItems([
+                NavigationItem::make('Volver al sistema')
+                    ->url(url('/dashboard'))
+                    ->icon('heroicon-o-arrow-left')
+                    ->sort(1), // Ajusta el orden en el menú
+            ])
             ->default()
             ->id('admin')
             ->path('admin')
@@ -53,6 +60,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);;
+            ]);
     }
 }
