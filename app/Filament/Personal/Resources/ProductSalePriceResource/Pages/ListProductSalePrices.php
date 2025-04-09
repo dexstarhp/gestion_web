d73524984384934e97666b02b3ad0eedea7bcdfc
Elmer <infcle@gmail.com>
2025-04-08 22:53:27 -0400

    feacture: Seq quita el boton de agregar item

--- app/Filament/Personal/Resources/ProductSalePriceResource/Pages/ListProductSalePrices.php
+++ app/Filament/Personal/Resources/ProductSalePriceResource/Pages/ListProductSalePrices.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Personal\Resources\ProductSalePriceResource\Pages;
 
 use App\Filament\Personal\Resources\ProductSalePriceResource;
-use Filament\Actions\Action;
 use Filament\Resources\Pages\ListRecords;
 
 class ListProductSalePrices extends ListRecords
@@ -13,11 +12,6 @@ class ListProductSalePrices extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Action::make('Registrar Producto')
-                ->icon('heroicon-o-plus')
-                ->label('Registrar Producto')
-                ->url(route('filament.admin.resources.products.create'))
-                ->color('primary'),
         ];
     }
 }
