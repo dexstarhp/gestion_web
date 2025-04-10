0b48f7072890c332af502b900a8b36a9c67557ff
Elmer Coronel <elmer.coronel@kernotec.com>
2025-04-10 16:54:56 -0400

    facture: quitando rutas Se tiene errores

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,32 +14,39 @@
                                     <p class="mb-0">Ingrese su correo electrónico y contraseña para iniciar sesión</p>
                                 </div>
                                 <div class="card-body">
-                                    <form role="form" method="POST" action="{{ route('login.perform') }}">
+                                    <form role="form" method="POST" action="{{ route('login') }}">
                                         @csrf
                                         @method('post')
                                         <div class="flex flex-col mb-3">
-                                            <input type="email" name="email" class="form-control form-control-lg" value="{{ old('email') ?? '' }}" aria-label="Email">
-                                            @error('email') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
+                                            <input type="email" name="email" class="form-control form-control-lg"
+                                                   value="{{ old('email') ?? '' }}" aria-label="Email">
+                                            @error('email') <p
+                                                    class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
                                         </div>
                                         <div class="flex flex-col mb-3">
-                                            <input type="password" name="password" class="form-control form-control-lg" aria-label="Password" >
-                                            @error('password') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
+                                            <input type="password" name="password" class="form-control form-control-lg"
+                                                   aria-label="Password">
+                                            @error('password') <p
+                                                    class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
                                         </div>
                                         <div class="form-check form-switch">
-                                            <input class="form-check-input" name="remember" type="checkbox" id="rememberMe">
+                                            <input class="form-check-input" name="remember" type="checkbox"
+                                                   id="rememberMe">
                                             <label class="form-check-label" for="rememberMe">Recuerdame</label>
                                         </div>
                                         <div class="text-center">
-                                            <button type="submit" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Inicia sesión</button>
+                                            <button type="submit" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">
+                                                Inicia sesión
+                                            </button>
                                         </div>
                                     </form>
                                 </div>
                             </div>
                         </div>
                         <div
-                            class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
+                                class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
                             <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
-                                style="background-image: url('{{ asset('img/paloma.png') }}'); background-size: cover;">
+                                 style="background-image: url('{{ asset('img/paloma.png') }}'); background-size: cover;">
                                 <span class="mask "></span>
                                 <h4 class="mt-5 text-white font-weight-bolder position-relative">"Bienvenidos"</h4>
                                 <p class="text-white position-relative">Funeraria la Redencion.</p>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PdfMovementDetailController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
@@ -17,29 +18,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-/*Route::get('/', function () {
-    return redirect('/dashboard');
-})->middleware('auth');*/
+Route::get('/', function () {
+    return redirect('/login');
+});
 
-/*Route::get('/login', [LoginController::class, 'show'])->middleware('guest')->name('login');
-Route::post('/login', [LoginController::class, 'login'])->middleware('guest')->name('login.perform');
-Route::get('/reset-password', [ResetPassword::class, 'show'])->middleware('guest')->name('reset-password');
-Route::post('/reset-password', [ResetPassword::class, 'send'])->middleware('guest')->name('reset.perform');
-Route::get('/change-password', [ChangePassword::class, 'show'])->middleware('guest')->name('change-password');
-Route::post('/change-password', [ChangePassword::class, 'update'])->middleware('guest')->name('change.perform');
-Route::get('/dashboard', [HomeController::class, 'index'])->name('home')->middleware('auth');*/
+Route::get('/login', [LoginController::class, 'show'])->middleware('guest')->name('login');
+Route::post('/login',
+    [LoginController::class, 'login'])->middleware('guest')->name('login.perform');
 
-/*Route::group(['middleware' => 'auth'], function () {
-    Route::get('/virtual-reality', [PageController::class, 'vr'])->name('virtual-reality');
-    Route::get('/rtl', [PageController::class, 'rtl'])->name('rtl');
-    Route::get('/profile', [UserProfileController::class, 'show'])->name('profile');
-    Route::post('/profile', [UserProfileController::class, 'update'])->name('profile.update');
-    Route::get('/profile-static', [PageController::class, 'profile'])->name('profile-static');
-    Route::get('/sign-in-static', [PageController::class, 'signin'])->name('sign-in-static');
-    Route::get('/sign-up-static', [PageController::class, 'signup'])->name('sign-up-static');
-    Route::post('logout', [LoginController::class, 'logout'])->name('logout');
-
-});*/
 
 // nuevas rutas
 Route::group(['middleware' => ['auth']], function () {
@@ -48,7 +34,7 @@ Route::group(['middleware' => ['auth']], function () {
         ->name('personal.inventory.stock.detail.pdf');
 });
 
-// acceso a cualqier persona
+// acceso a cualquier persona
 
 Route::get('/products/{product}/technical-file',
     [ProductController::class, 'file'])->name('product.file');
