86b439d7f86ac3d18df576cd0f05be44c27c857b
Elmer <infcle@gmail.com>
2025-04-16 20:03:34 -0400

    feacture: add plugin filament-breezy

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -17,6 +17,7 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Jeffgreco13\FilamentBreezy\BreezyCore;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -54,7 +55,14 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
-            ->databaseTransactions();
+            ->databaseTransactions()
+            ->plugins([
+                BreezyCore::make()
+                    ->myProfile(
+                        shouldRegisterNavigation: true,
+                        navigationGroup: 'Configuración',
+                    )
+            ]);
 
     }
 }
