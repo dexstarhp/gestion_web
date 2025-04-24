dcbf0eaeee94e1f8cfd39cbde5ea812a47248761
Elmer <infcle@gmail.com>
2025-04-24 07:46:53 -0400

    feacture: correcion del modelo user para los controlar los permisos

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use BezhanSalleh\FilamentShield\Traits\HasPanelShield;
-use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,24 +11,12 @@ use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
 
-class User extends Authenticatable implements FilamentUser
+class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, TwoFactorAuthenticatable;
 
     use HasRoles;
     use HasPanelShield;
-
-//    public function canAccessPanel(Panel $panel): bool
-//    {
-//        /*return match ($panel->getId()) {
-//            'admin' => $this->hasRole(['super_admin', 'admin']),
-//            'personal' => $this->hasRole(['super_admin', 'vendedor', 'compras']),
-//            default => false,
-//        };*/
-//        $permission = 'access_' . $panel->getId();
-//
-//        return $this->hasPermissionTo($permission);
-//    }
 
     /**
      * The attributes that are mass assignable.
